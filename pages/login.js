20565b310696f4291df59b3184c881b638a0d0fe
Reynel Sanchez <reynelsanchez70@gmail.com>
2020-10-23 16:50:26 -0400

    first commit

---
+++ pages/login.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Button from 'react-bootstrap/Button'
+export default function Login() {
+    
+  
+    return (
+        <div style={{ 
+           
+            backgroundColor: "orange", 
+            textAlign : "center",
+            
+            }} >
+        <div   >
+        <h1>HOLA MUNDO</h1>
+        <h3>Introduzca su correo</h3>
+        <input></input>
+        <h3>Contraseña</h3>
+        <input></input>
+        
+        </div>
+        <div>
+        <Button variant="primary">Primary</Button>
+        </div>
+        </div>
+    )
+}
